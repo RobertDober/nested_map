6c96816908f34071f3341a33a8615bc5bb84bb53
RobertDober <robert.dober@gmail.com>
2021-08-21 11:49:53 +0200

    flatten doctests completed

--- lib/nested_map.ex
+++ lib/nested_map.ex
@@ -10,11 +10,24 @@ defmodule NestedMap do
     ### Flatting
 
 
-    iex(0)> flatten(%{}) # empty
-    []
+      iex(0)> flatten(%{}) # empty
+      []
 
-    iex(0)> flatten(%{a: 1, b: 2}) # flat
-    [{[:a], 1}, {[:b], 2}]
+      iex(1)> flatten(%{a: 1, b: 2}) # flat
+      [{[:a], 1}, {[:b], 2}]
+
+      iex(2)> map = %{
+      ...(2)>   a: 1,
+      ...(2)>   b: %{
+      ...(2)>      ["you", "can"] => %{
+      ...(2)>          "do" => "that",
+      ...(2)>          "if" => %{you: :want}
+      ...(2)>      },
+      ...(2)>      the_inevitable: 42},
+      ...(2)>   c: 2}
+      ...(2)> flatten(map) # Be aware that this syntax puts the symbol key
+      ...(2)>              # `the_inevitable` before the other keys!
+      [{[:a], 1}, {[:b, :the_inevitable], 42}, {[:b, ["you", "can"], "do"], "that"}, {[:b, ["you", "can"], "if", :you], :want}, {[:c], 2}]
 
   """
 
